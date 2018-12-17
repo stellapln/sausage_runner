cb7ae95714ccb3f0eab44e208f722c61b67046e5
bmantova <baptiste.mantovani@laposte.net>
2018-12-17 15:27:07 +0100

    COrrection CamEyes

--- GLImac-Template-build/assimp/revision.h
+++ GLImac-Template-build/assimp/revision.h
@@ -1,7 +1,7 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0x5d6957b
+#define GitVersion 0x53b1587
 #define GitBranch "master"
 
 #endif // ASSIMP_REVISION_H_INC
--- GLImac-Template/glimac/include/glimac/TrackballCamera.hpp
+++ GLImac-Template/glimac/include/glimac/TrackballCamera.hpp
@@ -31,9 +31,12 @@ class TrackballCamera
 class EyesCam : public TrackballCamera
 {
 	private:
-		_maxAngle = 90.0f;
+		float _maxAngle = 90.0f;
+		float _fAngleX;
+		float _fAngleY;
 		glm::vec3 _position = glm::vec3(0.0f,0.0f,0.0f);
 	public:
+		EyesCam(float ax=0.0f,float ay=0.0f):TrackballCamera(0.0,ax,ay){}
 		void setMaxAngle(float m)
 		{
 			_maxAngle = m;
@@ -55,8 +58,7 @@ class EyesCam : public TrackballCamera
 		void moveFront(const float delta){}
 
 		glm::mat4 getMatrixView() const{
-			glm::mat4 view = glm::translate(glm::mat4(),glm::vec3(0.0,0.0,-_fDistance));
-			view = glm::rotate(view,glm::radians(_fAngleX),glm::vec3(1.0,0.0,0.0));
+			glm::mat4 view  = glm::rotate(glm::mat4(),glm::radians(_fAngleX),glm::vec3(1.0,0.0,0.0));
 			view = glm::rotate(view,glm::radians(_fAngleY),glm::vec3(0.0,1.0,0.0));
 			view = glm::translate(view,_position);
 			return view;
