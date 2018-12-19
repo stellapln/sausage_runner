74d8abc77f1c6c174d7a19d72c8637615371bc68
bmantova <baptiste.mantovani@laposte.net>
2018-12-19 15:48:39 +0100

    Pour Stella

--- GLImac-Template-build/assimp/revision.h
+++ GLImac-Template-build/assimp/revision.h
@@ -1,7 +1,7 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0xa605e09
+#define GitVersion 0xd7e4ce6
 #define GitBranch "master"
 
 #endif // ASSIMP_REVISION_H_INC
