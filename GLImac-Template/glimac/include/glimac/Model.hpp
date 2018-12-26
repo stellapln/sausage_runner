c74fdd25489077ddb1baeba6a1cc6958d8aba449
bmantova <36837912+bmantova@users.noreply.github.com>
2018-12-26 22:59:53 +0100

    Update the commentaries

--- GLImac-Template/glimac/include/glimac/Model.hpp
+++ GLImac-Template/glimac/include/glimac/Model.hpp
@@ -22,9 +22,9 @@
    */
 
 struct Vertex {
-    glm::vec3 Position; //!< Position of the vertex
-    glm::vec3 Normal; //!< Normal of the vertex
-    glm::vec2 TexCoords; //<! Texture coordinate of the vertex
+    glm::vec3 Position; /*!< Position of the vertex*/
+    glm::vec3 Normal; /*!< Normal of the vertex*/
+    glm::vec2 TexCoords; /*<! Texture coordinate of the vertex*/
 };
 
 /*! \struct Texture
@@ -32,9 +32,9 @@ struct Vertex {
    *    Contains the basic information of a 3D texture
    */
 struct Texture {
-    unsigned int id; //!< Id of the texture in the GPU
-    std::string type;  //!< Type of the texture : diffuse / specular
-    aiString path; //!< Path of the texture
+    unsigned int id; /*!< Id of the texture in the GPU*/
+    std::string type;  /*!< Type of the texture : diffuse / specular*/
+    aiString path; /*!< Path of the texture*/
 };
 
 /*! \class Mesh
@@ -44,17 +44,17 @@ struct Texture {
 
 class Mesh {
     private:
-        unsigned int vao, vbo, ebo; //!< Vertex Array Object, Vertex Buffer Object, Element Buffer Object 
-        void setupMesh(); //!< Setup the mesh at his initialisation
+        unsigned int vao, vbo, ebo; /*!< Vertex Array Object, Vertex Buffer Object, Element Buffer Object*/
+        void setupMesh(); /*!< Setup the mesh at his initialisation*/
     public:
-        std::vector<Vertex> _vertices; //!< Vertex of the mesh
-        std::vector<unsigned int> _indices; //!< Index of the vertex
-        std::vector<Texture> _textures; //!< Textures of the mesh
+        std::vector<Vertex> _vertices; /*!< Vertex of the mesh*/
+        std::vector<unsigned int> _indices; /*!< Index of the vertex*/
+        std::vector<Texture> _textures; /*!< Textures of the mesh*/
 
         Mesh(std::vector<Vertex> vert, std::vector<unsigned int> ind, std::vector<Texture> tex):_vertices(vert),_indices(ind),_textures(tex){
             setupMesh();
         };
-        void draw() const; //!< Bind the VAO and the textures and send its
+        void draw() const; /*!< Bind the VAO and the textures and send its*/
 };
 
 /*! \class Model
@@ -63,18 +63,18 @@ class Mesh {
    */
 class Model{
     private:
-        std::vector<Mesh> meshes; //!< Stock the meshs of the model
-        std::string directory; //!< Directory of the model
+        std::vector<Mesh> meshes; /*!< Stock the meshs of the model*/
+        std::string directory; /*!< Directory of the model*/
 
-        void loadModel(std::string path); //!< Load a model from his path
-        void processNode(aiNode *node, const aiScene *scene); //!< Browse all the nodes of the model
-        Mesh processMesh(aiMesh *mesh, const aiScene *scene); //!< Process a Mesh from a node of the model
-        std::vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type, std::string typeName); //<! Load a Material 
+        void loadModel(std::string path); /*!< Load a model from his path*/
+        void processNode(aiNode *node, const aiScene *scene); /*!< Browse all the nodes of the model*/
+        Mesh processMesh(aiMesh *mesh, const aiScene *scene); /*!< Process a Mesh from a node of the model*/
+        std::vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type, std::string typeName); /*<! Load a Material */
     public:
         Model(std::string path){
             loadModel(path);
         }
-        void draw() const; //!< Draw all the mesh of the model
+        void draw() const; /*!< Draw all the mesh of the model*/
 };
 
 #endif
