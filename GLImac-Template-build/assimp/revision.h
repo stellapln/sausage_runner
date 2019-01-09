eb7e86ed36d3c3d6675e1af94b85aaa3da232094
bmantova <baptiste.mantovani@laposte.net>
2019-01-09 13:56:43 +0100

    Pareil qu'avant

--- GLImac-Template-build/assimp/revision.h
+++ GLImac-Template-build/assimp/revision.h
@@ -1,7 +1,7 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0xc3a20b2
+#define GitVersion 0x418578a
 #define GitBranch "master"
 
 #endif // ASSIMP_REVISION_H_INC
