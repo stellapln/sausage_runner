b3964aba09bf2ad20ebdb9420395ee50f263823b
Lou93160 <36150676+Lou93160@users.noreply.github.com>
2019-01-10 10:08:01 +0100

    Update RandomMap.hpp

--- GLImac-Template/glimac/include/glimac/RandomMap.hpp
+++ GLImac-Template/glimac/include/glimac/RandomMap.hpp
@@ -10,33 +10,23 @@
 
 namespace sausageRunner {
 
-	std::string random_tile(int *last_broken, int *last_virage){
-	}
+	std::string random_tile(int *last_broken, int *last_virage);
 
-	std::string random_obs(int *last_mob){
-	}
+	std::string random_obs(int *last_mob);
 
-	std::string random_xobs(std::string obs){
-	}
+	std::string random_xobs(std::string obs);
 
-	std::string random_bonus(int *last_bonus, std::string obs){
-	}
+	std::string random_bonus(int *last_bonus, std::string obs);
 
-	std::string random_xbon(std::string bonus, std::string obs, std::string xobs){
+	std::string random_xbon(std::string bonus, std::string obs, std::string xobs);
 
-	}
+	std::string random_ybon(std::string bonus, std::string bloc, std::string obs);
 
-	std::string random_ybon(std::string bonus, std::string bloc, std::string obs){
-	}
+	std::string random_xcoin(std::string bloc, std::string obs, std::string xobs);
 
-	std::string random_xcoin(std::string bloc, std::string obs, std::string xobs){
-	}
+	std::string random_ycoin(std::string bonus, std::string bloc, std::string obs);
 
-	std::string random_ycoin(std::string bonus, std::string bloc, std::string obs){
-	}
-
-	void random_map(){
-	}
+	void random_map();
 }
 
 #endif
