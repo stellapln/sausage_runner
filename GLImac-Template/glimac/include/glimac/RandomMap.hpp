e7bdbf1ed89c543762bb13e47d074a192be766e7
Lou93160 <36150676+Lou93160@users.noreply.github.com>
2019-01-10 11:39:21 +0100

    Update RandomMap.hpp

--- GLImac-Template/glimac/include/glimac/RandomMap.hpp
+++ GLImac-Template/glimac/include/glimac/RandomMap.hpp
@@ -10,22 +10,49 @@
 
 namespace sausageRunner {
 
+	/*!
+	 * \brief Create a random tile, paying attention to last obstacles
+	 */
 	std::string random_tile(int *last_broken, int *last_virage, int *last_mob);
 
+	/*!
+	 * \brief Create a random tile, paying attention to last obstacles
+	 */
 	std::string random_obs(int *last_mob, std::string bloc, int *last_broken, int *last_virage);
 
+	/*!
+	 * \brief Create a random tile, paying attention to last obstacles
+	 */
 	std::string random_xobs(std::string obs);
 
+	/*!
+	 * \brief Create a random tile, paying attention to last obstacles
+	 */
 	std::string random_bonus(int *last_bonus, std::string obs);
 
+	/*!
+	 * \brief Create a random tile, paying attention to last obstacles
+	 */
 	std::string random_xbon(std::string bonus, std::string obs, std::string xobs);
 
+	/*!
+	 * \brief Create a random tile, paying attention to last obstacles
+	 */
 	std::string random_ybon(std::string bonus, std::string bloc, std::string obs);
 
+	/*!
+	 * \brief Create a random tile, paying attention to last obstacles
+	 */
 	std::string random_xcoin(std::string bloc, std::string obs, std::string xobs);
 
+	/*!
+	 * \brief Create a random tile, paying attention to last obstacles
+	 */
 	std::string random_ycoin(std::string xcoin, std::string bloc, std::string obs);
 
+	/*!
+	 * \brief Calling the differents fonctions to create the random map
+	 */
 	void random_map();
 }
 
